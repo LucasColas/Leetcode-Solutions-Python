342f19a60cc44ceec0bca206c9ded3f52da29cac
Lucas Colas <51634013+LucasColas@users.noreply.github.com>
2022-04-10 10:36:37 +0200

    Create 682 - Baseball Game.py

---
+++ 682 - Baseball Game.py
@@ -0,0 +1,37 @@
+"""
+
+You are keeping score for a baseball game with strange rules. The game consists of several rounds, where the scores of past rounds may affect future rounds' scores.
+
+At the beginning of the game, you start with an empty record. You are given a list of strings ops, where ops[i] is the ith operation you must apply to the record and is one of the following:
+
+    An integer x - Record a new score of x.
+    "+" - Record a new score that is the sum of the previous two scores. It is guaranteed there will always be two previous scores.
+    "D" - Record a new score that is double the previous score. It is guaranteed there will always be a previous score.
+    "C" - Invalidate the previous score, removing it from the record. It is guaranteed there will always be a previous score.
+
+Return the sum of all the scores on the record.
+From Leetcode : https://leetcode.com/problems/baseball-game/
+
+"""
+
+class Solution:
+    def calPoints(self, ops: List[str]) -> int:
+        
+        score = []
+        
+        for op in ops:
+            if op == "+":
+                score.append(score[-1] + score[-2])
+                
+            elif op == "D":
+                score.append(2*score[-1])
+            
+            elif op == "C":
+                score.pop()
+                
+            else:
+                score.append(int(op))
+                
+        
+        return sum(score)
+        
