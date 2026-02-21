b1591f8d37db711bccf82423a86e9bf1d29754ad
Lucas Colas <51634013+LucasColas@users.noreply.github.com>
2026-02-21 10:04:56 -0500

    Implement caching for prime number checks
    Optimize prime number checking by caching results.

--- 762 - Prime Number of Set Bits in Binary Representation.py
+++ 762 - Prime Number of Set Bits in Binary Representation.py
@@ -1,14 +1,16 @@
 class Solution:
     
-    
+    prime_numbers = {1:False}
 
     def is_prime_number(self, num : int) -> bool:
-        if num == 1:
-            return False
+        if num in self.prime_numbers:
+            return Solution.prime_numbers[num]
         for i in range(2, int(num ** 0.5)+1):
             if num % i == 0 and i != num:
+                Solution.prime_numbers[num] = False
                 return False
 
+        Solution.prime_numbers[num] = True
         return True
     def countPrimeSetBits(self, left: int, right: int) -> int:
         count = 0
